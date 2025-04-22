0467b77f418f478d679c7c53017e38a9ff3067a0
shaik-mabusubhani-gl <shaik.mabusubhani@globallogic.com>
2025-04-22 11:46:10 +0530

    Added the tags information

--- terraform/templates/network/vpc/outputs.tf
+++ terraform/templates/network/vpc/outputs.tf
@@ -12,3 +12,19 @@ output "private_subnets" {
   description = "IDs of the private subnets"
   value       = module.vpc.private_subnets
 }
+
+output "tags" {
+  description = "Tags which are applicable to all resources - map of `{key: value}` pairs"
+  value       = local.tags
+}
+
+output "asg_tags" {
+  description = "Tags which are appropriate auto scaling group (i.e. as a list of maps). See: https://www.terraform.io/docs/providers/aws/r/autoscaling_group.html"
+  value = [for key, val in local.tags :
+    {
+      key                 = key
+      value               = val
+      propagate_at_launch = true
+    }
+  ]
+}
