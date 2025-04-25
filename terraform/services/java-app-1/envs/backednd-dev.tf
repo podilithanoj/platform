69030c630ae76823e1cbea3f36dff163c50f824f
podilithanoj <psn.thanoj@gmail.com>
2025-04-25 10:17:59 +0530

    added

--- terraform/services/java-app-1/envs/backednd-dev.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "glacade"
-    key            = "java-app-1/dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
