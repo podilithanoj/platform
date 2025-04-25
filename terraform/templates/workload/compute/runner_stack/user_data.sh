78bf9492313d465f407bfdb418f65e8dfe3cb5a6
shaik-mabusubhani-gl <shaik.mabusubhani@globallogic.com>
2025-04-25 16:38:09 +0530

    added token

--- terraform/templates/workload/compute/runner_stack/user_data.sh
+++ terraform/templates/workload/compute/runner_stack/user_data.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
-apt-get update -y
-apt-get install -y git curl jq
 
+set -e
+
+# Update system
+apt-get update -y
+
+# Install dependencies
+apt-get install -y git curl jq unzip software-properties-common
+
+# ----------------------
+# Install JDK 17
+# ----------------------
+add-apt-repository -y ppa:openjdk-r/ppa
+apt-get update -y
+apt-get install -y openjdk-17-jdk
+
+# Verify JDK install
+java -version
+
+# ----------------------
+# Install Maven
+# ----------------------
+apt-get install -y maven
+mvn -version
+
+# ----------------------
+# Install JFrog CLI
+# ----------------------
+curl -fL https://releases.jfrog.io/artifactory/jfrog-cli/v2-jf/jfrog-cli-linux-amd64/ | tee jfrog > /dev/null
+chmod +x jfrog
+mv jfrog /usr/local/bin/
+
+# Verify JFrog CLI
+jfrog --version
+
+# ----------------------
 # GitHub Actions Runner Setup
+# ----------------------
 runner_version="2.308.0"
-mkdir /actions-runner && cd /actions-runner
+mkdir -p /actions-runner && cd /actions-runner
+
 curl -O -L https://github.com/actions/runner/releases/download/v${runner_version}/actions-runner-linux-x64-${runner_version}.tar.gz
 tar xzf ./actions-runner-linux-x64-${runner_version}.tar.gz
 
@@ -15,4 +50,5 @@ tar xzf ./actions-runner-linux-x64-${runner_version}.tar.gz
             --unattended \
             --labels terraform-runner
 
+# Optional: Run as a service or foreground
 ./run.sh
