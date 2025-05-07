b768376dc3a10d973c9c0702311b457673d9b65d
podilithanoj <101394210+podilithanoj@users.noreply.github.com>
2025-05-07 16:23:14 +0530

    Update backend-dev.tf

--- terraform/services/runner-stack/envs/backend-dev.tf
+++ terraform/services/runner-stack/envs/backend-dev.tf
@@ -1,5 +1,5 @@
 
     bucket         = "glacade"
     key            = "runner-stack/dev/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-2"
     encrypt        = true
